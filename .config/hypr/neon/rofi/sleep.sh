4e49f4e1bfbc4dad7718fc758974eb0eb648d499
Javier Gómez <frvnzj.gmz@gmail.com>
2023-02-19 11:01:46 -0600

    refactor: hyprconf
    Más simple la configuración de Hyprland

--- .config/hypr/neon/rofi/sleep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-swayidle -w \
-                timeout 320 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
-                    resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 2))' \
-                timeout 1200 "$HOME/.config/hypr/scripts/lock.sh & sleep 1 && hyprctl dispatch dpms off" \
-                    resume 'hyprctl dispatch dpms on' \
-                timeout 1800 'systemctl suspend'
