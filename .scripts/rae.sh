80f46e5ade5a26fdebcae6215e50a10ab4a6ecb4
frvnzj <frvnzj.gmz@gmail.com>
2023-07-03 13:08:31 -0600

    refactor: RAE
    Aclaraciones sobre el script rae.sh

--- .scripts/rae.sh
+++ .scripts/rae.sh
@@ -1,8 +1,17 @@
 #!/bin/dash
 
+#     ____  ___    ______
+#    / __ \/   |  / ____/
+#   / /_/ / /| | / __/   
+#  / _, _/ ___ |/ /___   
+# /_/ |_/_/  |_/_____/   
+
+# Script para buscar palabras en la RAE.
+# Dependencias: bemenu, firefox y librewolf.
+
 export BEMENU_OPTS='-H 24 -l 10 --fb "#1e1e2e" --ff "#94e2d5" --nb "#1e1e2e" --nf "#f5e0dc" --tb "#1e1e2e" --hb "#1e1e2e" --tf "#cba6f7" --hf "#89b4fa" --nf "#f5e0dc" --af "#f5e0dc" --ab "#1e1e2e" --fn "SF Pro 10"'
 
-rae=$(printf "" | bemenu -p " Diccionario 📖:")
+rae="${1:-$(printf "%s" | bemenu -p ' Diccionario 📖:')}"
 buscar=$(printf "🦊 firefox\n🐺 librewolf" | bemenu -p " Navegador 🌐" | awk '{print $2}')
 
 $buscar https://dle.rae.es/"$rae"
