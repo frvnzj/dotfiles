0ab5e742a1fcbee95a7b3ca9bc690906583c60a2
frvnzj <frvnzj.gmz@gmail.com>
2023-07-09 13:16:08 -0600

    feat(Plugins): adds
    Añadí los plugins colorizer y girsigns

---
+++ .config/nvim/lua/xJAVIERx/plugins/colorizer.lua
@@ -0,0 +1,5 @@
+return {
+    "NvChad/nvim-colorizer.lua",
+    cmd = { "ColorizerToggle", "ColorizerAttachToBuffer", "ColorizerDetachFromBuffer", "ColorizerReloadAllBuffers" },
+    opts = { user_default_options = { names = false } },
+}
