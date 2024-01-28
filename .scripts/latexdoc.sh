17901c876f00d84e688bb90136bf176cb69878c1
frvnzj <frvnzj.gmz@gmail.com>
2024-01-27 18:47:34 -0600

    betterscript
    Mejoré el script, distingue archivos de la documentación, cambio de grep por rg

--- .scripts/latexdoc.sh
+++ .scripts/latexdoc.sh
@@ -2,12 +2,14 @@
 
 export BEMENU_OPTS='-i -l 10 -H 24 --fb "#1e1e2e" --ff "#94e2d5" --nb "#1e1e2e" --nf "#f5e0dc" --tb "#1e1e2e" --hb "#1e1e2e" --tf "#cba6f7" --hf "#89b4fa" --nf "#f5e0dc" --af "#f5e0dc" --ab "#1e1e2e" --fn "SF Pro 10"'
 
-select_pkg=$(curl -s "https://www.ctan.org/json/2.0/packages" | jq '.' | grep -o '"key".*\|"caption".*' | sed 's/".*: \|"//g' | sed -z 's/,\n/ - /g' | bemenu -p 'LaTeX docs   : ' | awk '{print $1}')
+select_pkg=$(curl -s "https://www.ctan.org/json/2.0/packages" | jq '.' | rg -o '"key".*|"caption".*' | sed 's/".*: \|"//g' | sed -z 's/,\n/ - /g' | bemenu -p 'LaTeX docs   : ' | awk '{print $1}')
 
-_urls=$(curl -s "https://www.ctan.org/json/2.0/pkg/$select_pkg" | jq '.documentation' | grep -o '"details".*\|"href".*' | sed 's/".*: //g' | sed -z 's/",\n"/ - /g' | bemenu -p 'Docs' | grep -o '/.*[^"]')
+_urls=$(curl -s "https://www.ctan.org/json/2.0/pkg/$select_pkg" | jq '.documentation' | rg -o '"details".*|"href".*' | sed 's/".*: //g' | sed -z 's/",\n"/ - /g' | bemenu -p 'Docs' | rg -o '/.*[^"]')
 
-if echo "$_urls" | rg -q ".pdf" ; then
-  zathura https://mirrors.ctan.org"$_urls" &
+if echo "$_urls" | rg -q ".pdf"; then
+	zathura https://mirrors.ctan.org"$_urls" >/dev/null 2>&1
+elif echo "$_urls" | rg -q ".md|README|.txt"; then
+	wezterm -e nvim -R https://mirrors.ctan.org"$_urls" >/dev/null 2>&1
 else
-  wezterm -e nvim -R https://mirrors.ctan.org"$_urls" &
+	exit 1
 fi
