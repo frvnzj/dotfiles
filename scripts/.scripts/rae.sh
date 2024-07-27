1977193a32ca2bc9876227edfe31a571400a7777
frvnzj <frvnzj.gmz@gmail.com>
2024-07-26 20:13:56 -0600

    fuzzel
    fuzzel instead of rofi and bemenu

--- scripts/.scripts/rae.sh
+++ scripts/.scripts/rae.sh
@@ -1,4 +1,4 @@
-#!/bin/dash
+#!/bin/sh
 
 #     ____  ___    ______
 #    / __ \/   |  / ____/
@@ -9,10 +9,7 @@
 # Script para buscar palabras en la RAE.
 # Dependencias: fuzzel, firefox y librewolf.
 
-# rae="${1:-$(printf "%s" | fuzzel -d -l 0 -w 15 -y 4 -p 'Diccionario 📖:  ')}"
-# buscar=$(printf "🦊 firefox\n🐺 librewolf" | fuzzel -d -l 2 -w 10 -p " Navegador 🌐 " | awk '{print $2}')
-
-rae="${1:-$(printf "%s" | rofi -dmenu -replace -config ~/.config/rofi/config-rae.rasi -p 'Diccionario 📖')}"
-buscar=$(printf "🦊 firefox\n🐺 librewolf" | rofi -dmenu -replace -config ~/.config/rofi/config-browser.rasi -p "Navegador 🌐" | awk '{print $2}')
+rae="${1:-$(printf "%s" | fuzzel --dmenu -p "Diccionario > " -l 0 -w 10 -P 0)}"
+buscar=$(echo -en "firefox\0icon\x1ffirefox\nlibrewolf\0icon\x1flibrewolf" | fuzzel --dmenu -p "Navegador > " -l 2 -w 10 -P 10)
 
 $buscar https://dle.rae.es/"$rae"
