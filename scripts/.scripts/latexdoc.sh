7bdfd03cf5779c36d86ad45341e89cf878ab6d09
frvnzj <frvnzj.gmz@gmail.com>
2024-07-26 20:14:34 -0600

    latexdocs
    better script with fuzzel and icons

--- scripts/.scripts/latexdoc.sh
+++ scripts/.scripts/latexdoc.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
-export BEMENU_OPTS='-i -l 10 -H 24 --fb "#1e1e2e" --ff "#94e2d5" --nb "#1e1e2e" --nf "#f5e0dc" --tb "#1e1e2e" --hb "#1e1e2e" --tf "#cba6f7" --hf "#89b4fa" --nf "#f5e0dc" --af "#f5e0dc" --ab "#1e1e2e" --fn "SF Pro 10"'
+package_name=$(curl -s "https://www.ctan.org/json/2.0/packages" | jq '.' | rg -o '"key".*|"caption".*' | sed 's/".*: \|"//g' | sed -z 's/,\n/ - /g' | sed 's/$/\\0icon\\x1ftexstudio/g')
+select_pkg=$(printf "$package_name" | fuzzel -i --dmenu --line-height=24px -p 'Paquete  > ' -w 50 -l 20 -P 10 -B 0 --font="SF Pro:size=10" | awk '{print $1}')
 
-select_pkg=$(curl -s "https://www.ctan.org/json/2.0/packages" | jq '.' | rg -o '"key".*|"caption".*' | sed 's/".*: \|"//g' | sed -z 's/,\n/ - /g' | bemenu -p 'LaTeX docs   : ' | awk '{print $1}')
+doc_pkg=$(curl -s "https://www.ctan.org/json/2.0/pkg/$select_pkg" | jq '.documentation' | rg -o '"details".*|"href".*' | sed 's/".*: //g' | sed -z 's/",\n"/ - /g')
 
-_urls=$(curl -s "https://www.ctan.org/json/2.0/pkg/$select_pkg" | jq '.documentation' | rg -o '"details".*|"href".*' | sed 's/".*: //g' | sed -z 's/",\n"/ - /g' | bemenu -p "$select_pkg docs  : " | rg -o '/.*[^"]')
+ls_docs=$(
+  printf "$doc_pkg" | rg "README|.md|.txt" | sed 's/$/\\0icon\\x1fnvim/g'
+  printf "$doc_pkg" | rg ".html" | sed 's/$/\\0icon\\x1ffirefox/g'
+  printf "$doc_pkg" | rg ".pdf" | sed 's/$/\\0icon\\x1fdocument-viewer/g'
+)
+urls_pkg=$(printf "$ls_docs" | fuzzel -i --dmenu --line-height=24px -p "$select_pkg docs  > " -w 50 -l 4 -P 10 -B 0 --font="SF Pro:size=10" | rg -o '/.*[^"]')
 
-_link="https://mirrors.mit.edu/CTAN$_urls"
+_link="https://mirrors.mit.edu/CTAN$urls_pkg"
 
-if echo "$_link" | rg -q ".pdf"; then
-	zathura "$_link" > /dev/null &
-elif echo "$_link" | rg -q ".md|README|.txt"; then
-	curl -sL "$_link">"/tmp/$(echo "$_link" | sed 's|.*/||;s/%20/ /g')" && setsid -f wezterm start -- nvim -R "/tmp/$(echo "$_link" | sed 's|.*/||;s/%20/ /g')" > /dev/null &
+if printf "$_link" | rg -q ".pdf"; then
+  zathura "$_link" >/dev/null &
+elif printf "$_link" | rg -q ".md|README|.txt"; then
+  curl -sL "$_link" >"/tmp/$(printf "$_link" | sed 's|.*/||;s/%20/ /g')" && setsid -f wezterm start -- nvim -R "/tmp/$(echo "$_link" | sed 's|.*/||;s/%20/ /g')" >/dev/null &
+elif printf "$_link" | rg -q ".html"; then
+  firefox "$_link"
 else
-	exit 1
+  exit 1
 fi
